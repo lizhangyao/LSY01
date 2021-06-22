01633499356a8c118a3dc3b6e57a24e55b35feeb
lizhangyao <83164359+lizhangyao@users.noreply.github.com>
2021-06-22 10:48:16 +0800

    6.22
    6.22

---
+++ UITest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class UITest : MonoBehaviour
+{
+    public Button pauseBtn;
+    bool isPause = false;
+    public Slider masterVolueSlider;
+    public AudioMixer masterMixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseBtn.onClick.AddListener(PauseGame);
+        masterVolueSlider.onValueChanged.AddListener(VolumeChange);
+    }
+    public void VolumeChange(float volume)
+    {
+        masterMixer.SetFloat("masterVolume", volume);
+    }
+    public void PauseGame()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
